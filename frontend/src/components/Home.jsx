0ac1126414817473781fe8ccfdf761ed489fc764
aswinp333 <aswinp910@gmail.com>
2023-05-27 17:04:51 +0530

    all done

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>WELCOME TO THE APP</h1>
+        <br />
+        <h2>Your otp has been verified</h2>
+    </div>
+  )
+}
+
+export default Home
